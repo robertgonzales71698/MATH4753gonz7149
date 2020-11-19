14b073900ba5d2f1f1f10e5c656fadefa6bc6920
Robert Gonzales <robert.m.gonzales-1@ou.edu>
2020-11-18 21:04:28 -0600

    Added myboot2

---
+++ R/myboot2.R
@@ -0,0 +1,32 @@
+#' Bootstrp 2 function
+#'
+#' Bootstrap intervals function
+#'
+#' @param iter
+#' @param x
+#' @param fun
+#' @param alpha
+#' @param cx
+#' @param ...
+#'
+#' @return a Histogram of bootstrap sample statistics given the parameters
+#' @export
+#'
+#' @examples
+#' myboot2(10000,x=sam,fun="mean",alpha=0.05,xlab="mean",col="Purple",cx=1.5)
+myboot2<-function(iter=10000,x,fun="mean",alpha=0.05,cx=1.5,...){
+  n=length(x)
+  y=sample(x,n*iter,replace=TRUE)
+  rs.mat=matrix(y,nr=n,nc=iter,byrow=TRUE)
+  xstat=apply(rs.mat,2,fun)
+  ci=quantile(xstat,c(alpha/2,1-alpha/2))
+  para=hist(xstat,freq=FALSE,las=1, main=paste("Histogram of Bootstrap sample statistics","\n","alpha=",alpha," iter=",iter,sep=""), ...)
+  mat=matrix(x,nr=length(x),nc=1,byrow=TRUE)
+  pte=apply(mat,2,fun)
+  abline(v=pte,lwd=3,col="Black")
+  segments(ci[1],0,ci[2],0,lwd=4)
+  text(ci[1],0,paste("(",round(ci[1],2),sep=""),col="Red",cex=cx)
+  text(ci[2],0,paste(round(ci[2],2),")",sep=""),col="Red",cex=cx)
+  text(pte,max(para$density)/2,round(pte,2),cex=cx)
+  invisible(list(ci=ci,fun=fun,x=x))
+}
